b6f7b5a821829a5b608c9981dc53c3b07261f8c3
tambu <alberto.tamburini@hotmail.it>
2020-02-22 17:53:02 +0100

    Added info

--- AutoGenMiFare.py
+++ AutoGenMiFare.py
@@ -8,7 +8,7 @@ import chamtool
 
 def main():
     desc="A program to automatize the charge of the dump.bin into the Chameleon, change of UID and than the elaboration of the log resulting from a failed challange-response."
-    desc2=" It is important that chamlog.py and chamtool.py are located in the same directory of this file."
+    desc2=" It is important that chamlog.py, chamtool.py, the 'Chamelon' directory and 'EM4233_010Editor_Template.bt' are located in the same directory of this file."
     argParser=argparse.ArgumentParser(description=desc+desc2)
     argGroup=argParser.add_argument_group(title="Command list")
     argGroup.add_argument("-u", "--uid", dest="uid", required=True, nargs=1, help="The UID of the new MiFare card")    
