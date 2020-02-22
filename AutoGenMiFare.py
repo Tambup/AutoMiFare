927186f54a75a15aa41ba4e615d8b87aca15fca4
Stoppini <rove.albe@gmail.com>
2020-02-22 16:16:31 +0100

    prova tambu

--- AutoGenMiFare.py
+++ AutoGenMiFare.py
@@ -6,6 +6,7 @@ import argparse
 
 
 def main():
+    ciao tambu
     desc="A program to automatize the charge of the dump.bin into the Chameleon, change of UID and than the elaboration of the log resulting from a failed challange-response."
     desc2=" It is important that chamlog.py and chamtool.py are located in the same directory of this file."
     argParser=argparse.ArgumentParser(description=desc+desc2)
